9d02c4e40873268c24fb9539177632ee9a97f9bd
victorparkh <victor.parkh@gmail.com>
2021-05-05 20:13:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+s = 10;
+r = 20;
+h = 30;
+v = Math.PI * r * r * h;
+
+document.write("<p>**************</p>");
+document.write(
+  `<p>Обьем цилиндра с площадью основы S = ${s}, радиусом R = ${r} и высотой H = ${h} равен:</p>`
+);
+document.write("<p>--------------------</p>");
+document.write(`V = ${v}`);
+document.write("<p>-------------------</p>");
+document.write("<p>end.</p>");
